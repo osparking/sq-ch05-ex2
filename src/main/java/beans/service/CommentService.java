89e23f7572e4d188fd78c1fd433ef8132effb4b0
osparking <jbpark03@gmail.com>
2023-09-04 17:36:54 +0900

    D.I. approach for singletone bean

--- src/main/java/beans/service/CommentService.java
+++ src/main/java/beans/service/CommentService.java
@@ -1,14 +1,17 @@
 package beans.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import beans.repository.MariaRepository;
 
 @Service
 public class CommentService {
-	@Autowired
 	private MariaRepository repository;
+
+	public CommentService(MariaRepository repository) {
+		super();
+		this.repository = repository;
+	}
 
 	public MariaRepository getRepository() {
 		return repository;
